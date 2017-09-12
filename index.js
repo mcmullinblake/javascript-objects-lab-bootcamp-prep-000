2e8e7688c7fefbaae46cc68536764641b0562bbd
blake mcmullin <mcmullin.blake@gmail.com>
2017-09-12 00:54:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-var recipes = new Object();
+var recipes = new Object()
 
-function updateObjectWithKeyAndValue(object, key, value) {
-  var obj = { prop: 1 }
-  return Object.assign(recipes, {[prop]: 1 })
-}
